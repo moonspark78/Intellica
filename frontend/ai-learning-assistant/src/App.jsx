a36780442dcdc932d9939a0ec11b4fbdd21cfbe5
Omar Jabour <jabour.omar@hotmail.com>
2026-01-03 00:42:44 +0100

    :construction: frontend : working on App  :construction:

--- frontend/ai-learning-assistant/src/App.jsx
+++ frontend/ai-learning-assistant/src/App.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom' 
+import RegisterPage from './pages/Auth/RegisterPage';
+import LoginPage from './pages/Auth/LoginPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const App = () => {
+  const isAuthenticated = false;
+  const loading = false;
+
+  if (loading) {
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <p>Loading...</p>
+      </div>
+    )
+  }
+
+
+
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route 
+          path='/' 
+          element={isAuthenticated ? <Navigate to='/dashboard' replace /> : <Navigate to='/login' replace />} 
+        />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
+
+        <Route path="*" element={<NotFoundPage/>}/>
+      </Routes>
+    </Router>
   )
 }
 
