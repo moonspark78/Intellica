a7c617c1ce5699a7d50f4464d7cb94b9636cc9f6
Omar Jabour <jabour.omar@hotmail.com>
2026-01-11 14:09:19 +0100

    :construction: backend: Working on authRoutes  ./  :construction:

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,3 +7,9 @@ import {
     updateProfile,
     changePassword
 } from "../controllers/authController.js";
+
+import protect from "../middleware/auth.js";
+
+const router = express.Router();
+
+// Validation middleware
