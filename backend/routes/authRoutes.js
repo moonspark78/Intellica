a59a842f619479de3463bd7d85ab64d021d28abf
Omar Jabour <jabour.omar@hotmail.com>
2026-01-11 14:10:43 +0100

    :construction: backend: Working on authRoutes  .  :construction:

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -13,3 +13,9 @@ import protect from "../middleware/auth.js";
 const router = express.Router();
 
 // Validation middleware
+const registerValidation = [
+    body('username')
+        .trim()
+        .isLength({ min: 3 })
+        .withMessage('Username must be at least 3 characters long'),
+]
