ff5334c823b535dba444bf4156328c823685336d
Omar Jabour <jabour.omar@hotmail.com>
2026-01-17 19:16:15 +0100

    :art: Backend:  create the first model ./

--- backend/models/User.js
+++ backend/models/User.js
@@ -10,4 +10,16 @@ const userSchema = new mongoose.Schema({
         trim: true,
         minlength: [3, 'Username must be at least 3 characters long'],
     },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/, 'Please provide a valid email address'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [6, 'Password must be at least 6 characters long'],
+    }
 });
