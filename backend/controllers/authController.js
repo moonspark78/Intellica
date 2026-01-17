28c6d9f2975b6f39c95da084a6c5723b3eea49c9
Omar Jabour <jabour.omar@hotmail.com>
2026-01-17 18:13:19 +0100

    :art: Backend: working on authControlller , fct generateToken

---
+++ backend/controllers/authController.js
@@ -0,0 +1,9 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+// Generate JWT Token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN || '7d',
+    });
+};
