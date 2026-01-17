579b9043fb519776101e28042b0b896643062f8b
Omar Jabour <jabour.omar@hotmail.com>
2026-01-17 17:07:50 +0100

    :art: Backend: working on registerValidation

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -18,4 +18,11 @@ const registerValidation = [
         .trim()
         .isLength({ min: 3 })
         .withMessage('Username must be at least 3 characters long'),
-]
+    body('email')
+        .isEmail()
+        .normalizeEmail()
+        .withMessage('Please provide a valid email address'),
+    body('password')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long')
+];
