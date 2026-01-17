5dcdc874176efd1071bf158b542ac31adb13fdb3
Omar Jabour <jabour.omar@hotmail.com>
2026-01-17 19:22:37 +0100

    :art: Backend:  create the first model ./ working on Hash password before saving

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
-import { use } from 'react';
+
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -21,5 +21,21 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Password is required'],
         minlength: [6, 'Password must be at least 6 characters long'],
+        select: false,
+    },
+    profileImage: {
+        type: String,
+        default: null
     }
+    }, { 
+    timestamps: true
+});
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
