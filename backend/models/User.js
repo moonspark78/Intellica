34acedf42afefd780bf910d1c7a7bc488e290171
Omar Jabour <jabour.omar@hotmail.com>
2026-01-17 19:26:00 +0100

    :art: Backend:  create the first model ./ working on Compare passwords method

--- backend/models/User.js
+++ backend/models/User.js
@@ -39,3 +39,12 @@ userSchema.pre('save', async function (next) {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 });
+
+// Compare passwords method
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
