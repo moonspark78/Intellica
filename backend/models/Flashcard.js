2db53fd3129b8c593eeb998cf746c6999044a9a2
Omar Jabour <jabour.omar@hotmail.com>
2026-01-19 19:18:43 +0100

    :art: Backend:  . working on Flashcard  .

--- backend/models/Flashcard.js
+++ backend/models/Flashcard.js
@@ -2,8 +2,16 @@ import mongoose from "mongoose";
 import { use } from "react";
 
 const flashcardSchema = new mongoose.Schema({
-    userId: {},
-    documentId: {},
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    documentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Document',
+        required: true
+    },
     cards:[
         {},
     ]
