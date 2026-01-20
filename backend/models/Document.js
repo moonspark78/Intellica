2301ea8217e4de9f2a56d853686fd044c8a92a25
Omar Jabour <jabour.omar@hotmail.com>
2026-01-20 18:42:07 +0100

    :art: Backend:  . working on Documents schema   .

---
+++ backend/models/Document.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const documentSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {},
+    fileName: {},
+    filePath: {},
+});
