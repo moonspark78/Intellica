0ef2857ba5a72daf13d475e6e749614dae94184d
Omar Jabour <jabour.omar@hotmail.com>
2026-01-24 13:17:39 +0100

    :art: Backend:  working on auth.js  :art:  /

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,3 +1,19 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
+const protect = async (req, res, next) => {
+    let token;
+
+    // Check for token in Authorization header
+    if ( req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try{
+            token = req.headers.authorization.split(' ')[1];
+
+            // Verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            // Get user from the token
+            req.user = await User.findById(decoded.id).select('-password');
+        }
+    }
+
+};
