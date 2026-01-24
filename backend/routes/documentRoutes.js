7b9a08cfca981f23e225f07307cdee0baa9b2cc9
Omar Jabour <jabour.omar@hotmail.com>
2026-01-24 22:22:51 +0100

    :art: Backend:  working on DocumentsRoutes  :art: /

--- backend/routes/documentRoutes.js
+++ backend/routes/documentRoutes.js
@@ -1,6 +1,11 @@
 import espress from 'express';
 import {
     uploadDocument,
+    getDocuments,
+    getDocument,
+    updateDocument,
+    deleteDocument
 } from '../controllers/documentController.js';
+import protect from '../middleware/auth.js';
 
 const router = espress.Router();
