f0da7f230debffe61820227272dfacd45096f082
Omar Jabour <jabour.omar@hotmail.com>
2026-01-25 15:43:14 +0100

    :art: Backend:  working on DocumentsRoutes .  :art:

--- backend/routes/documentRoutes.js
+++ backend/routes/documentRoutes.js
@@ -10,3 +10,8 @@ import protect from '../middleware/auth.js';
 import upload from "../config/multer.js"
 
 const router = espress.Router();
+
+// All routes are protected
+router.use(protect);
+
+router.post('/upload', upload.single('file'), uploadDocument);
