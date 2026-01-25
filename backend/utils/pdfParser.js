27bc7ccd9a6c68c109ab33bd0173c43ff0111a04
Omar Jabour <jabour.omar@hotmail.com>
2026-01-25 16:22:59 +0100

    :art: Backend:  working on PDFparser  :art:  /

--- backend/utils/pdfParser.js
+++ backend/utils/pdfParser.js
@@ -1,3 +1,9 @@
 import fs from 'fs/promises';
 import { pdfParse } from 'pdf-parse';
 
+/**
+ * Extract text from PDF file
+ * @param {string} filePath - Path to the PDF file
+ * @returns {Promise<{text: string, numPages: number}>}
+ */
+
