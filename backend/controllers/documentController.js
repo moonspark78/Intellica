8878c3f5d3d9655b85b8124bd94883da0aade01c
Omar Jabour <jabour.omar@hotmail.com>
2026-01-25 16:04:06 +0100

    :art: Backend:  working on DocumentsControlllers ./  :art:

--- backend/controllers/documentController.js
+++ backend/controllers/documentController.js
@@ -5,3 +5,11 @@ import {extractTextFromPDF} from '../utils/pdfParser.js';
 import {chunkText} from '../utils/textChunker.js';
 import fs from 'fs/promises';
 import mongoose from "mongoose";
+
+
+// @desc   Upload PDF document
+// @route  POST /api/documents/upload
+// @access Private
+export const uploadDocument = async (req, res, next) => {
+    try {} catch (error) {}
+};
