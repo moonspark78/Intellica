18651b77c71425386ff39c16c600914e6e0ad423
Omar Jabour <jabour.omar@hotmail.com>
2026-01-31 21:45:19 +0100

    :art: Backend:  working on FlashcardRoutes / :art:

--- backend/routes/flashcardRoutes.js
+++ backend/routes/flashcardRoutes.js
@@ -1,3 +1,21 @@
 import express from 'express';
-import {} from "../controllers/flashcardController.js";
+import {
+    getFlashcards,
+    getAllFlashcardsSets,
+    reviewFlashcard,
+    toggleStarFlashcard,
+    deleteFlashcardSet,
+} from "../controllers/flashcardController.js";
 import { protect } from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.use(protect);
+
+router.get("/", getAllFlashcardsSets);
+router.get("/:documentId", getFlashcards);
+router.post("/:cardId/review", reviewFlashcard);
+router.put("/:cardId/star", toggleStarFlashcard);
+router.delete("/:id", deleteFlashcardSet);
+
+export default router;
