df329d7156ad81b7e7faf2e102b18f41d6d87c3a
Omar Jabour <jabour.omar@hotmail.com>
2026-01-31 22:21:38 +0100

    :art: Backend:  working on FlashcardRoutes  :art:

--- backend/controllers/flashcardController.js
+++ backend/controllers/flashcardController.js
@@ -6,7 +6,18 @@ import Flashcard from "../models/Flashcard";
 // @access Private
 export const getFlashcards = async (req, res, next) => {
     try{
+        const flashcards = await Flashcard.find({
+            userId: req.user._id,
+            documentId: req.params.documentId
+        })
+          .populate('documentId', 'title fileName')
+          .sort({ createdAt: -1 });
 
+        res.status(200).json({
+            success: true,
+            count: flashcards.length,
+            data: flashcards
+        });
     } catch (error) {
         next(error);
     }
@@ -17,24 +28,44 @@ export const getFlashcards = async (req, res, next) => {
 // @route GET /api/flashcards
 // @access Private
 export const getAllFlashcardsSets = async (req, res, next) => {
-    
+    try {
+
+    } catch (error) {
+        next(error);
+    }
 };
 
 // @desc Mark flashcard as reviewed
 // @route POST /api/flashcards/:cardId/review
 // @access Private
 export const reviewFlashcard = async (req, res, next) => {
+    try {
 
+    } catch (error) {
+        next(error);
+    }
 }
 
 
 // @desc Toggle star/favorite on flashcard
 // @route PUT /api/flashcards/:cardId/star
 // @access Private
-export const toggleStarFlashcard = async (req, res, next) => {};
+export const toggleStarFlashcard = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error);
+    }
+};
 
 
 // @desc Delete a flashcard set
 // @route DELETE /api/flashcards/:id
 // @access Private
-export const deleteFlashcardSet = async (req, res, next) => {};
+export const deleteFlashcardSet = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error);
+    }
+};
