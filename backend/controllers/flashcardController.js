981d97b5a52b35b08f2a1653ef7a93d825767951
Omar Jabour <jabour.omar@hotmail.com>
2026-01-31 22:01:22 +0100

    :art: Backend:  working on FlashcardRoutes  :art:

---
+++ backend/controllers/flashcardController.js
@@ -0,0 +1,13 @@
+import Flashcard from "../models/Flashcard";
+
+
+// @desc Get all flashcards for a document
+// @route GET /api/flashcards/document/:documentId
+// @access Private
+export const getFlashcards = async (req, res, next) => {
+    try{
+
+    } catch (error) {
+        next(error);
+    }
+};
