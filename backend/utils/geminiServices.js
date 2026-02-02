f7e949f21ac03dfa33b86f6136c26210e60b4bc1
Omar Jabour <jabour.omar@hotmail.com>
2026-02-02 20:51:28 +0100

    :art: Backend:  working on :memo: geminiServices  :art:

--- backend/utils/geminiServices.js
+++ backend/utils/geminiServices.js
@@ -3,9 +3,17 @@ import { GoogleGenAI } from "@google/genai";
 
 dotenv.config();
 
-const ai = new GoogleGenAI({ apiKey: process.env.GENAI_API_KEY });
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
-if (!process.env.GENAI_API_KEY) {
-    console.error("FATAL ERROR: GENAI_API_KEY is not set in environment variables.");
+if (!process.env.GEMINI_API_KEY) {
+    console.error("FATAL ERROR: GEMINI_API_KEY is not set in environment variables.");
     process.exit(1);
 }
+
+
+/**
+ *  Generate flaadcards from text
+ *  @param {string} text - Document text
+ *  @param {number} count - Number of flashcards to generate
+ *  @returns {Promise<Array<{question: string, answer: string, difficulty: string}>>} -
+ * */
