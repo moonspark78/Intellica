9f3e86902c382b5a38029cc2f4cd31af4fa0e7ba
Omar Jabour <jabour.omar@hotmail.com>
2026-02-02 19:43:40 +0100

    :art: Backend:  working on :memo: aiController  :art: /

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -4,3 +4,8 @@ import Quiz from '../models/Quiz.js';
 import ChatMessage from '../models/ChatMessage.js';
 import * as geminiService from '../utils/geminiServices.js';
 import { findRelevantChunks } from '../utils/textChunker.js'
+
+
+// @desc      Generate flashcards from a document
+// @route     POST /api/ai/generate-flashcards
+// @access    Private
