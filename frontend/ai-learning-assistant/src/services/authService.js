b2249af426ad5582640ac2cb4bae0ef67b52f1a8
Omar Jabour <jabour.omar@hotmail.com>
2026-02-27 17:02:46 +0100

    :art: Front:  working on :memo: AxiosInstance .   :art:

--- frontend/ai-learning-assistant/src/services/authService.js
+++ frontend/ai-learning-assistant/src/services/authService.js
@@ -1,2 +1,11 @@
 import axiosInstance from '../utils/axiosInstance';
 import { API_PATHS } from '../utils/apiPaths';
+
+const login = async (email, password) => {
+    try {
+        
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || { message: 'An unknown error occurred.' };
+    }
+};
