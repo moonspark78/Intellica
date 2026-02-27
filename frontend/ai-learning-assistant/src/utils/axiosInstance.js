fc537948cd08fd2012fcc45e95c590170ef3f7cf
Omar Jabour <jabour.omar@hotmail.com>
2026-02-27 16:46:55 +0100

    :art: Front:  working on :memo: AxiosInstance ./././   :art:

--- frontend/ai-learning-assistant/src/utils/axiosInstance.js
+++ frontend/ai-learning-assistant/src/utils/axiosInstance.js
@@ -14,5 +14,12 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
     (config) => {
         const accessToken = localStorage.getItem("token");
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
 );
