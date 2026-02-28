c3f31b28392c77dfae36931a720888b10c3401e3
Omar Jabour <jabour.omar@hotmail.com>
2026-02-28 02:00:04 +0100

    :art: Front:  working on :memo: authServices  /././././.   :art:

--- frontend/ai-learning-assistant/src/services/authService.js
+++ frontend/ai-learning-assistant/src/services/authService.js
@@ -12,3 +12,16 @@ const login = async (email, password) => {
         throw error.response?.data || { message: 'An unknown error occurred.' };
     }
 };
+
+const register = async (username, email, password) => {
+    try {
+        const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+            username,
+            email,
+            password
+        });
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || { message: 'An unknown error occurred.' };
+    }
+};
