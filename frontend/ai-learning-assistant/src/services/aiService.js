f547610a8a560c12c992e8ae12da57f46e02b3b1
Omar Jabour <jabour.omar@hotmail.com>
2026-03-02 18:16:42 +0100

    :construction: working on aiService ././/../.  :construction:

--- frontend/ai-learning-assistant/src/services/aiService.js
+++ frontend/ai-learning-assistant/src/services/aiService.js
@@ -3,7 +3,8 @@ import { API_PATHS } from '../utils/apiPaths';
 
 const generateFlashcards = async (documentId, options) => {
     try {
-        
+        const response = await axiosInstance.post(API_PATHS.AI.GENERATE_FLASHCARDS, {documentId, ...options});
+        return response.data;
     } catch (error) {
         throw error.response?.data || { message: 'An error occurred while generating flashcards.' };
     }
