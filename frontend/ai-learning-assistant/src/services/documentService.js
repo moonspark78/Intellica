82886fe4f04e41c5141d9307e89d073b184a626b
Omar Jabour <jabour.omar@hotmail.com>
2026-03-04 01:18:18 +0100

    :truck: working  on DocumentsService  :truck:

---
+++ frontend/ai-learning-assistant/src/services/documentService.js
@@ -0,0 +1,5 @@
+import axiosInstance from "../utils/axiosInstance";
+import { API_PATHS } from "../utils/apiPaths";
+import { getDocument } from '../../../../backend/controllers/documentController';
+
+const getDocuments = async () => {};
