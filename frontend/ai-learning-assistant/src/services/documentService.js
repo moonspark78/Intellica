dd53f3d12171a2155f2c47d1d4b7acbcb3a51ba4
Omar Jabour <jabour.omar@hotmail.com>
2026-03-04 01:19:50 +0100

    :truck: working  on DocumentsService ./././/../  :truck:

--- frontend/ai-learning-assistant/src/services/documentService.js
+++ frontend/ai-learning-assistant/src/services/documentService.js
@@ -2,4 +2,10 @@ import axiosInstance from "../utils/axiosInstance";
 import { API_PATHS } from "../utils/apiPaths";
 import { getDocument } from '../../../../backend/controllers/documentController';
 
-const getDocuments = async () => {};
+const getDocuments = async () => {
+    try {
+        
+    } catch (error) {
+        throw error.response?.data || { message: 'Failed to fetch documents.' };
+    }
+};
