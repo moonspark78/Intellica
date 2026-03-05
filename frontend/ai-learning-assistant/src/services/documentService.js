2ad9b02bb7598cb3c867153bb14108813a41ac03
Omar Jabour <jabour.omar@hotmail.com>
2026-03-06 00:11:30 +0100

    :truck: working on documentService.js   :truck:

--- frontend/ai-learning-assistant/src/services/documentService.js
+++ frontend/ai-learning-assistant/src/services/documentService.js
@@ -1,6 +1,5 @@
 import axiosInstance from "../utils/axiosInstance";
 import { API_PATHS } from "../utils/apiPaths";
-import { getDocument } from '../../../../backend/controllers/documentController';
 
 const getDocuments = async () => {
     try {
@@ -24,6 +23,13 @@ const uploadDocument = async (formData) => {
     }
 };
 
+const deleteDocument = async (id) => {
+    try {
+        
+    } catch (error) {
+        throw error.response?.data || { message: 'Failed to delete document.' };
+    }
+};
 
 
 const documentService = {
