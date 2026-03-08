7d19050adb716c971b02e822ef8e1763e6f117f4
Omar Jabour <jabour.omar@hotmail.com>
2026-03-08 18:39:35 +0100

    :truck: Frontend:  working on quizService.js  ././.  :truck:

--- frontend/ai-learning-assistant/src/services/quizService.js
+++ frontend/ai-learning-assistant/src/services/quizService.js
@@ -3,9 +3,10 @@ import { API_PATHS } from "../utils/apiPaths";
 
 const getQuizzesForDocument = async (documentId) => {
     try {
-        
+        const response = await axiosInstance.get(API_PATHS.QUIZZES.GET_QUIZZES_FOR_DOC(documentId));
+        return response.data;
     } catch (error) {
-        
+        throw error.response?.data || { message: 'Failed to fetch quizzes.' };
     }
 };
 
