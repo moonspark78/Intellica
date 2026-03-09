711753536068896df9767a36a614ed08435c2a59
Omar Jabour <jabour.omar@hotmail.com>
2026-03-09 14:40:32 +0100

    :truck: Frontend:  working on progressService.js  /./.  :truck:

--- frontend/ai-learning-assistant/src/services/progressService.js
+++ frontend/ai-learning-assistant/src/services/progressService.js
@@ -3,8 +3,9 @@ import { API_PATHS } from "../utils/apiPaths";
 
 const getDashboardData = async () => {
     try {
-        
+        const response = await axiosInstance.get(API_PATHS.PROGRESS.GET_DASHBOARD);
+        return response.data;
     } catch (error) {
-        
+        throw error.response?.data || { message: 'Failed to fetch dashboard data.' };
     }
 };
