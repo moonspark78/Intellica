5ce5dd9dd46ac3592258957ed84e1602ee6d8223
Omar Jabour <jabour.omar@hotmail.com>
2026-03-09 14:39:48 +0100

    :truck: Frontend:  working on progressService.js  /././  :truck:

--- frontend/ai-learning-assistant/src/services/progressService.js
+++ frontend/ai-learning-assistant/src/services/progressService.js
@@ -1,2 +1,10 @@
 import axiosInstance from "../utils/axiosInstance";
 import { API_PATHS } from "../utils/apiPaths";
+
+const getDashboardData = async () => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
