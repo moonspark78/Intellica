d7d884630e35ef6012ceb8bb17617cefe8bb4849
Omar Jabour <jabour.omar@hotmail.com>
2026-03-10 12:29:23 +0100

    :truck: Frontend:  workingprogres on LoginPage  :truck:

--- frontend/ai-learning-assistant/src/pages/Auth/LoginPage.jsx
+++ frontend/ai-learning-assistant/src/pages/Auth/LoginPage.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
+import { toast } from 'react-hot-toast';
+import { BrainCircuit, Mail, Lock, ArrowRight } from 'lucide-react';
+import toast from 'react-hot-toast';
 
 const LoginPage = () => {
+
+  const [email, setEmail] = useState('test@program.com');
+  const [password, setPassword] = useState('Test@1234');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [focusedField, setFocusedField] = useState(null);
+
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
   return (
     <div>LoginPage</div>
   )
