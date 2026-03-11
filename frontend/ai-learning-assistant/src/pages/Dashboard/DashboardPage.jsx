ff1d6906c1ffd0ae3f02dad1c02c1f7b714769db
Omar Jabour <jabour.omar@hotmail.com>
2026-03-11 14:20:14 +0100

    :truck: Frontend:  working on Dashboard ./././././   :truck:

--- frontend/ai-learning-assistant/src/pages/Dashboard/DashboardPage.jsx
+++ frontend/ai-learning-assistant/src/pages/Dashboard/DashboardPage.jsx
@@ -5,6 +5,23 @@ import toast from "react-hot-toast";
 import { FileText, BookOpen, BrainCircuit, TrendingUp, Clock } from "lucide-react";
 
 const DashboardPage = () => {
+
+  const [dashboardData, setDashboardData] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchDashboardData = async () => {
+      try {
+        
+      } catch (error) {
+        
+      }
+    };
+  },[])
+
+
+
+
   return (
     <div>DashboardPage</div>
   )
