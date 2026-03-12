0cec57a56d1a16207a2215158100406493d1ee07
Omar Jabour <jabour.omar@hotmail.com>
2026-03-12 15:19:02 +0100

    :truck: Frontend:  working on Dashboard DocumentListPage ././.   :truck:

--- frontend/ai-learning-assistant/src/pages/Documents/DocumentListPage.jsx
+++ frontend/ai-learning-assistant/src/pages/Documents/DocumentListPage.jsx
@@ -2,7 +2,30 @@ import React, {useEffect, useState} from 'react'
 import { Plus, Upload, Trash2, FileText, X } from "lucide-react";
 import toast from "react-hot-toast";
 
+import documentService from '../../services/documentService';
+import Spinner from '../../components/common/Spinner';
+
+
 const DocumentListPage = () => {
+
+  const [documents, setDocuments] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // State for upload modal
+  const [isUploadModalOpen, setIsUploadModalOpen] = useState(false);
+  const [uploadFile,setUploadFile] = useState(null);
+  const [uploadTitle,setUploadTitle] = useState("");
+  const [uploading,setUploading] = useState(false);
+
+  const fetchDocuments = async () => {
+    try {
+      
+    } catch (error) {
+      
+    }
+  };
+
+
   return (
     <div>DocumentListPage</div>
   )
