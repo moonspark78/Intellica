13e6c6fe73f42e289ec340e4b73f8a1043931312
Omar Jabour <jabour.omar@hotmail.com>
2026-03-15 00:58:15 +0100

    :truck: Frontend:  working on Dashbord ChatInterfacePage   :truck:

---
+++ frontend/ai-learning-assistant/src/components/flashcards/Flashcard.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const Flashcard = ({ flashcard, onToggleStar }) => {
+
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleFlip = () => {
+        setIsFlipped(!isFlipped);
+    };
+
+  return <div>Flashcard</div>;
+}
+
+export default Flashcard
--- frontend/ai-learning-assistant/src/pages/Documents/DocumentDetailPage.jsx
+++ frontend/ai-learning-assistant/src/pages/Documents/DocumentDetailPage.jsx
@@ -7,6 +7,8 @@ import { ArrowLeft, ExternalLink } from 'lucide-react'
 import PageHeader from '../../components/common/PageHeader';
 import Tabs from '../../components/common/Tabs';
 import ChatInterface from "../../components/chat/ChatInterface"
+import AIActions from '../../components/ai/AIActions'
+import FlashcardManager from '../../components/flashcards/FlashcardManager'
 
 
 const DocumentDetailPage = () => {
@@ -89,11 +91,11 @@ const DocumentDetailPage = () => {
   };
 
   const renderAIActions = () => {
-    return "renderAIActions"
+    return <AIActions/>
   };
 
   const renderFlashcardsTab = () => {
-    return "renderFlashcardsTab"
+    return <FlashcardManager documentId={id} />
   };
 
   const renderQuizzesTab = () => {
