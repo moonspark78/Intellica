369dfdb9ca253b69cc3e9ee78a63752bc2094c1c
Omar Jabour <jabour.omar@hotmail.com>
2026-03-15 16:04:01 +0100

    :truck: Frontend:  working on Dashbord FlashcardManager   :truck:

--- frontend/ai-learning-assistant/src/components/flashcards/FlashcardManager.jsx
+++ frontend/ai-learning-assistant/src/components/flashcards/FlashcardManager.jsx
@@ -20,8 +20,40 @@ import Flashcard from './Flashcard';
 
 
 
-
 const FlashcardManager = ({documentId}) => {
+
+  const [flashcardSets, setFlashcardSets] = useState([]);
+  const [selectedSet, setSelectedSet] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [generating, setGenerating] = useState(false);
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [deleting, setDeleting] = useState(false);
+  const [setToDelete, setSetToDelete] = useState(null);
+
+
+  const fetchFlashcardSets = async () => {
+    setLoading(true);
+    try {
+      const response = await flashcardService.getFlashcardsForDocument(
+        documentId
+      );
+      setFlashcardSets(response.data);
+    } catch (error) {
+      toast.error("Failed to fetch flashcard sets.");
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (documentId) {
+      fetchFlashcardSets();
+    }
+  }, [documentId])
+
+
   return (
     <div>FlashcardManager</div>
   )
