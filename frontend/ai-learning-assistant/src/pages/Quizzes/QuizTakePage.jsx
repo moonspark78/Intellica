eb315f73f680711868451d984b84d832a3744f91
Omar Jabour <jabour.omar@hotmail.com>
2026-03-19 11:43:07 +0100

    :truck: Frontend:  working on Dashbord QuizManager.jsx   :truck:

--- frontend/ai-learning-assistant/src/pages/Quizzes/QuizTakePage.jsx
+++ frontend/ai-learning-assistant/src/pages/Quizzes/QuizTakePage.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { ChevronLeft, ChevronRight, CheckCircle2 } from 'lucide-react'
+import quizService from '../../services/quizService'
+import PageHeader from '../../components/common/PageHeader'
+import Spinner from '../../components/common/Spinner'
+import toast from 'react-hot-toast'
+import Button from '../../components/common/Button'
+
+
+
 
 const QuizTakePage = () => {
+
+  const {quizId} = useParams();
+  const navigate = useNavigate();
+  const [quiz, setQuiz] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+  const [submitting, setSubmitting] = useState(false);
+
   return (
     <div>QuizTakePage</div>
   )
