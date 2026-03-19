a27032a954666e25414da938626729a8bab38eb6
Omar Jabour <jabour.omar@hotmail.com>
2026-03-19 09:20:43 +0100

    :truck: Frontend:  working on Dashbord QuizManager.jsx ././././  :truck:

--- frontend/ai-learning-assistant/src/components/common/EmptyState.jsx
+++ frontend/ai-learning-assistant/src/components/common/EmptyState.jsx
@@ -4,8 +4,8 @@ import { Plus, FileText } from 'lucide-react';
 
 const EmptyState = ({title, description,buttonText, onActionClick}) => {
   return (
-    <div className=''>
-        <div className="">
+    <div className='flex flex-col items-center justify-center py-16 px-6 text-center bg-linear-to-br from-slate-50/50 to-white border-2 border-dashed border-slate-200 rounded-3xl'>
+        <div className="inline-flex items-center justify-center w-16 h-16 rounded-2xl bg-linear-to-br from-slate-100 to-slate-200/50 mb-6">
             <FileText className='' strokeWidth={2}/>
         </div>
         <h3 className=''>{title}</h3>
