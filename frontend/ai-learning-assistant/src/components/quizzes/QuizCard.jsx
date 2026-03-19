299ad8d91074bb4c502ad9b2b75fe3f0e9213271
Omar Jabour <jabour.omar@hotmail.com>
2026-03-19 09:47:58 +0100

    :truck: Frontend:  working on Dashbord QuizManager.jsx ./  :truck:

--- frontend/ai-learning-assistant/src/components/quizzes/QuizCard.jsx
+++ frontend/ai-learning-assistant/src/components/quizzes/QuizCard.jsx
@@ -1,9 +1,23 @@
 import React from "react"
+import {Link} from "react-router-dom"
+import { Trash2, Play, BarChart2, Award } from "lucide-react"
+import { moment } from 'moment';
+
 
 
 const QuizCard = ({quiz, onDelete}) => {
   return (
-    <div>QuizCard</div>
+    <div className="">
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          onDelete(quiz)
+        }}
+        className=""
+      >
+
+      </button>
+    </div>
   )
 }
 
